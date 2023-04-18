d4ff4a4bbd4d5482eddc1ef87f165f6ba66b36eb
Raphael Peuckert <raphael@spdigital.agency>
2023-04-18 20:54:18 +0200

    home--about section finished for desktop

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,10 @@ export default function Home() {
                 <img className='home--hero-img' src={waterBottleImg} alt="" />
                 <a href='/shop' className='home--hero-cta'>shop now</a>
             </section>
+            <section className="home--about">
+                <h4>What is ØNESIP®?</h4>
+                <p><span className='text--brand-name'>ØNESIP®</span> provides premium water for a healthy lifestyle. Our goal is to offer you the cleanest and most refreshing water possible. We're dedicated to sourcing the highest quality water and using advanced filtration and purification methods to ensure it's free of harmful contaminants. Our water is perfect for athletes, health enthusiasts, and anyone who wants a delicious and healthy way to stay hydrated. Choose <span className='text--brand-name'>ØNESIP®</span> for the best water available and to help you lead an active and healthy lifestyle.</p>
+            </section>
         </div>
     )
 }
